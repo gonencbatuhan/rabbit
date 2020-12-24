304f1c176284a4ae16652a49e7adcb6faf216300
batuhangonenc <gonencbthn@gmail.com>
2020-12-24 03:27:35 +0300

    improved by regex controls

--- rabbit.py
+++ rabbit.py
@@ -10,7 +10,7 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-import sys,os
+import sys,os,re
 from random import randint as rInt
 from time import time
 #date-generator
@@ -140,8 +140,12 @@ while 1:
 
 				key = input("----------\nenter a key :")
 
-				if not(key.count("-")==9):
+				keypattern = r"[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}-[^0-9][0-9]{1,2}"
+
+				match = re.match(keypattern,key)
+				if not(match):
 					ct -= 1
+					print("\nwrong key format.")
 					continue
 
 				break
